dfdf0ddddbd24b20eeacba83ef97eec068eb4148
Rodrigo Gonçalves de Queiroz <rodrigo.goncalves@DBC-001068.local>
2022-11-16 21:25:29 -0300

    configuraçoes iniciais

---
+++ marvelSuperHeroes/marvelSuperHeroes/Sources/Main/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  marvelSuperHeroes
+//
+//  Created by Rodrigo Gonçalves de Queiroz on 16/11/22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
